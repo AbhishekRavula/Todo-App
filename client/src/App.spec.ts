c30c213e9b8088e2836f459833069a1513f8cea6
abhishek <abhishek.ravula@gsynergy.com>
2023-07-11 18:38:26 +0530

    spec for integration testing of Todo functionality

---
+++ client/src/App.spec.ts
@@ -0,0 +1,7 @@
+describe("Todo", () => {
+  it("it should render todo list", () => {});
+  it("it should add new todo item", () => {});
+  it("it should update todo item", () => {});
+  it("it should mark todo item as completed", () => {});
+  it("it should delete todo item", () => {});
+});
