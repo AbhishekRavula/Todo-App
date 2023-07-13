0556747462777f0c5ddfdfe3e242e90d38f77a61
abhishek <abhishek.ravula@gsynergy.com>
2023-07-13 12:13:24 +0530

    added spec for todo inbout and updated spec for  todo item and todo list

---
+++ client/src/components/TodoInput/TodoInput.spec.ts
@@ -0,0 +1,6 @@
+describe("Todo Input", () => {
+  it("should show input field and add button", () => {});
+  it("should disabled add button when input field is empty", () => {});
+  it("should enable add button when input field is not empty", () => {});
+  it("should call callback function for add button click", () => {});
+});
--- client/src/components/TodoList/TodoList.spec.ts
+++ client/src/components/TodoList/TodoList.spec.ts
@@ -1,4 +1,4 @@
 describe("Todo List", () => {
   it("it should render todo items", () => {});
-  it("it should render empty help text when list is empty", () => {});
+  it("it should render empty help text when todo list is empty", () => {});
 });
