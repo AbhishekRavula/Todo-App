19ead74ccea559746ab8443bd5b237aa34bcd59a
abhishek <abhishek.ravula@gsynergy.com>
2023-07-14 12:35:40 +0530

    changed client test coverages to test directory

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,5 +12,9 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
     css: true,
+    coverage: {
+      provider: "v8",
+      reportsDirectory: "./src/test/coverage",
+    },
   },
 });
